b5b93e249eb1940d669881f2f972028ac400dac7
Ujivl <90654578+Ujivl@users.noreply.github.com>
2024-11-14 01:39:22 -0500

    bleh

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
 public class Main {
-
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Draggable and Enlarging Square");
-        postitnote draggableSquare = new postitnote();
-        frame.add(draggableSquare);
-        frame.setSize(800, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        new MindMapApp();
     }
 }
